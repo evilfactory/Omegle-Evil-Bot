58b727050d9c2495e1eea2e6b419f8716e6f7fcf
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2020-05-29 10:30:44 -0300

    update database and convert.js

--- word2vec/convert.js
+++ word2vec/convert.js
@@ -25,7 +25,7 @@ fs.readFile(__dirname+'/../data/database.json', 'utf8', function (err, data) {
         }
     }
 
-    /*
+    
     fs.writeFile(__dirname+"/../data/input.txt",rawdata, function (err) {
         if (err) throw err;
 
@@ -39,25 +39,26 @@ fs.readFile(__dirname+'/../data/database.json', 'utf8', function (err, data) {
 
             console.log("Converting data to vectors...")
 
+            w2v.word2vec( __dirname + "/../data/phrases.txt", __dirname + '/../data/vectors.txt', {
+                cbow: 1,
+                size: 200,
+                window: 8,
+                negative: 25,
+                hs: 0,
+                sample: 1e-4,
+                threads: 20,
+                iter: 15,
+                minCount: 2
+            });
+
+            
+
         });
         
 
     });
 
-    */
-
-
-    w2v.word2vec( __dirname + "/../data/phrases.txt", __dirname + '/../data/vectors.txt', {
-        cbow: 1,
-        size: 200,
-        window: 8,
-        negative: 25,
-        hs: 0,
-        sample: 1e-4,
-        threads: 20,
-        iter: 15,
-        minCount: 2
-    });
+    
 
 });
 
